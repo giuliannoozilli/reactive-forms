b8096ee2ccd36cf09fb836a8f393a5ec2b8a38f5
Giulianno Zilli <gzilli01@hotmail.com>
2021-10-18 10:10:19 -0300

    dd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'reactive-forms';
+  onSubmit(f) {
+    console.log(f.value);
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ReactiveformsComponent } from './reactiveforms/reactiveforms.component';
+import { TemplateformsComponent } from './templateforms/templateforms.component';
+import { AngularFireModule } from '@angular/fire';
+const firebaseConfig = {
+  apiKey: "AIzaSyCsf73klr67ApRMfDUYGK9FETF2oalQK8Q",
+  authDomain: "reactive-forms-8b262.firebaseapp.com",
+  projectId: "reactive-forms-8b262",
+  storageBucket: "reactive-forms-8b262.appspot.com",
+  messagingSenderId: "694349386856",
+  appId: "1:694349386856:web:410cca5d73613d91af97dd",
+  measurementId: "G-S4WG3VFP7H"
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReactiveformsComponent,
+    TemplateformsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AngularFireModule.initializeApp(config)
   ],
   providers: [],
   bootstrap: [AppComponent]
